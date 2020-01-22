377fc9c364ca9fad21e5485faf23bcb410561fde
gaus7724 <gaus7724@PCB-LAB360-22>
2020-01-22 12:54:20 -0600

    Created variables for firstName, lastName, and favorite number. Also derived crazyNumber.

--- Variables3/Variables3/Program.cs
+++ Variables3/Variables3/Program.cs
@@ -10,6 +10,27 @@ namespace Variables3
     {
         static void Main(string[] args)
         {
+            //create a string named firstName
+            string firstName;
+
+            //assign a value to firstName (use your name)
+            firstName = "Gillian";
+
+            //create an int named favoriteNumber and assign your favorite number to it
+            int favoriteNumber = 21;
+
+            double crazyNumber = 7.6767 * favoriteNumber;
+
+            string lastName = "Gauss";
+
+            string fullName = firstName + " " + lastName;
+
+            Console.WriteLine(fullName + " has the favorite number of " + favoriteNumber + "" +
+                "\nwhich equates to a crazy number of " + crazyNumber);
+
+            //MAKE IT DYNAMIC
+
+            Console.ReadKey();
         }
     }
 }
