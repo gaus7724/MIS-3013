055f56e2d74437e56db73575e4ad33177cb2b72e
gaus7724 <gaus7724@PCB-LAB360-22>
2020-02-19 13:06:59 -0600

    Participation 4

---
+++ Part 4/Part 4/Program.cs
@@ -0,0 +1,62 @@
+﻿//Gillian Gauss
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double scores;
+            string scoreAsString;
+            string answer;
+            double max = 0;
+            double min = 1000;
+            double totals = 0;
+            double avgScore;
+
+            List<double> scoreList = new List<Double>();
+
+
+
+            do
+            {
+                Console.WriteLine("Please enter your test scores.");
+                scoreAsString = Console.ReadLine();
+                scores = Convert.ToInt32(scoreAsString);
+                scoreList.Add(scores);
+
+                if (scores > max)
+                {
+                    max = scores;
+                }
+
+                if (scores < min)
+                {
+                    min = scores;
+                }
+
+               Console.WriteLine("Do you have more test scores to enter? Y or N. ");
+                answer = Console.ReadLine();
+
+            } while (answer.ToUpper() == "Y" );
+
+            for (int i = 0; i < scoreList.Count; i++)
+            {
+                totals = totals + scoreList[i];
+              
+            }
+            avgScore = totals / scoreList.Count;
+
+            Console.WriteLine("Your test score average is " + avgScore);
+            Console.WriteLine("Your minimum test score is " + min);
+            Console.WriteLine("Your maximum test score is " + max);
+
+            Console.ReadKey();
+        }
+    }
+}
