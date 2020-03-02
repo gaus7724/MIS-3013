84a46ff4e65e3aa2af8b952a3c432b7599e437d6
gaus7724 <gaus7724@PCB-LAB360-22>
2020-03-02 12:43:21 -0600

    Question 1

---
+++ Participations/March2 Participation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace March2_Participation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Question 1
+
+            string cs1 = "";
+            string cs2 = "";
+            string cs3 = "";
+            string cn1 = "";
+            string cn2 = "";
+            string cn3 = "";
+            
+
+            Console.WriteLine("Please enter your first course subject.");
+            cs1 = Console.ReadLine();
+
+            Console.WriteLine("Please enter your second course subject.");
+            cs2 = Console.ReadLine();
+
+            Console.WriteLine("Please enter your third course subject.");
+            cs3 = Console.ReadLine();
+
+            Console.WriteLine("Please enter your first course number");
+            cn1 = Console.ReadLine();
+
+            Console.WriteLine("Please enter your second course number");
+            cn2 = Console.ReadLine();
+
+            Console.WriteLine("Please enter your third course number");
+            cn3 = Console.ReadLine();
+
+            string[] subjectArray = { cs1, cs2, cs3 };
+            string[] numberArray = { cn1, cn2, cn3 };
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(subjectArray[i] + numberArray[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Participations/March2 Participation/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("March2 Participation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("March2 Participation")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9357ae30-78cc-41a3-8a36-72daf9a1d3b8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
