2115c9fb9686448c5914f8fff87af5c992d1b513
gaus7724 <gaus7724@PCB-LAB360-22>
2020-03-02 13:10:17 -0600

    Worked on Q3

---
+++ Participation March 2/Question 3/Program.cs
@@ -0,0 +1,27 @@
+﻿//Gillian Gauss
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string courseCode = "";
+            double finalGrade = 0;
+
+            Dictionary<string, double> student = new Dictionary<string, double>();
+
+            student.Add("MIS313", 90);
+            student.Add("ACCT3013", 80);
+            student.Add("BAD4013", 75);
+
+
+
+        }
+    }
+}
