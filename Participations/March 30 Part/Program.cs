6c6136f56869782d29d214c35df4f85b2d5af089
Gillian Gauss <gillianlizabeth@gmail.com>
2020-03-30 12:33:08 -0500

    March 30

---
+++ Participations/March 30 Part/Program.cs
@@ -0,0 +1,21 @@
+﻿//Gillian Gauss
+using System;
+
+namespace March_30_Part
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextBox TB1 = new TextBox();
+            TB1.Padding = 3;
+            TB1.Text = "This is the first text box.";
+            TB1.BackColor = ConsoleColor.Yellow;
+            TB1.ForeColor = ConsoleColor.Red;
+
+            
+            Console.ReadKey();
+        }
+       
+    }
+}
---
+++ Participations/March 30 Part/TextBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace March_30_Part
+{
+    class TextBox
+    {
+        public int Padding { get; set;}
+
+        public ConsoleColor BackColor { get; set;}
+
+        public ConsoleColor ForeColor { get; set; }
+
+        public String Text { get; set; }
+
+        public TextBox()
+        {
+            Padding = 1;
+            BackColor = ConsoleColor.Black;
+            ForeColor = ConsoleColor.White;
+            Text = "sample text";
+        }
+
+        public void DisplayText()
+        {
+            string space = " ";
+
+            Console.ForegroundColor = ForeColor;
+            Console.BackgroundColor = BackColor;
+
+            for (int i = 0; i < Padding; i++)
+            {
+                space = space + " ";
+            }
+            Console.WriteLine(Text);
+        }
+
+    }
+}
