b9ad1f041e65735d2135e2e3735e59aaa1601fcc
Gillian Gauss <gillianlizabeth@gmail.com>
2020-04-23 13:04:30 -0500

    done

--- Participations/WPFPart2/MainWindow.xaml.cs
+++ Participations/WPFPart2/MainWindow.xaml.cs
@@ -24,5 +24,37 @@ namespace WPFPart2
         {
             InitializeComponent();
         }
+
+       
+
+
+        private void btn_Odds_Checked(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Add(1);
+            listBox.Items.Add(3);
+            listBox.Items.Add(5);
+            listBox.Items.Add(7);
+            listBox.Items.Add(9);
+
+        }
+
+        private void txt_Max_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        
+
+        }
+
+        private void btn_Even_Checked(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Add(2);
+            listBox.Items.Add(4);
+            listBox.Items.Add(6);
+            listBox.Items.Add(8);
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Clear();
+        }
     }
 }
